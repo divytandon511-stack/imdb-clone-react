15d535d1a2945ad1093c8aefa078979ba8304505
Harsh <harshmaurya140@gmail.com>
2026-04-23 19:58:54 +0530

    Added HeroBanner and PromoBanner components

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -43,10 +43,10 @@ export default function HeroBanner() {
             alt="Featured content"
           />
 
-          {/* Gradient overlay */}
+ 
           <div className={styles.gradientOverlay} />
 
-          {/* Bottom title bar */}
+
           <div className={styles.titleBar}>
             <div className={styles.titleLeft}>
               <button className={styles.prevBtn}>&#8249;</button>
@@ -63,14 +63,14 @@ export default function HeroBanner() {
             </div>
           </div>
 
-          {/* Play button */}
+ 
           <button className={styles.playBtn}>
             <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
               <polygon points="5 3 19 12 5 21 5 3"/>
             </svg>
           </button>
 
-          {/* Thumbnail strip */}
+
           <div className={styles.thumbnailStrip}>
             <button className={styles.addThumb}>
               <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
@@ -89,7 +89,7 @@ export default function HeroBanner() {
         </div>
       </div>
 
-      {/* Up Next Panel */}
+
       <div className={styles.upNext}>
         <h3 className={styles.upNextTitle}>Up next</h3>
 
